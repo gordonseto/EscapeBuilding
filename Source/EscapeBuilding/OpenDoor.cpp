56d960b65953c0c3bd53033e0b25967a0bb32df4
Gordon Seto <gordon.seto@gmail.com>
2017-03-15 22:55:50 -0700

    added wall

--- Source/EscapeBuilding/OpenDoor.cpp
+++ Source/EscapeBuilding/OpenDoor.cpp
@@ -22,7 +22,7 @@ void UOpenDoor::BeginPlay()
     
     AActor* Owner = GetOwner();
     
-    FRotator NewRotation = FRotator(0.0f, -90.0f, 0.0f);
+    FRotator NewRotation = FRotator(0.0f, -180.0f, 0.0f);
     
     Owner->SetActorRotation(NewRotation);
 }
